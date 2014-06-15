1e5d80cf40071e2a2cdf3332ffd27d5dd2ead087
Lowell Bander <lowellbander@gmail.com>
2014-06-15 14:05:07 -0700

    runs locally

--- run.py
+++ run.py
@@ -1,7 +1,23 @@
+import settings
 from flask import Flask
+from mongoengine import connect
+from flask.ext.mongoengine import MongoEngine
+from flask.ext.mongorest import MongoRest
 
 app = Flask(__name__)
 app.debug = True
+
+HOST = 'mongodb://' + settings.USER + ':' + settings.PASSWORD +'@kahana.mongohq.com:' + str(settings.PORT) + '/' + settings.APP,
+
+app.config["MONGODB_DB"] = 'app26394662'
+connect(
+        'app26394662',
+        host=HOST,
+        port=10014
+)
+
+db = MongoEngine(app)
+api = MongoRest(app)
 
 @app.route('/')
 def index():
---
+++ settings.py
@@ -0,0 +1,4 @@
+USER = 'lowell'
+PASSWORD = 'bander'
+PORT = 10029
+APP = 'app26412283'
